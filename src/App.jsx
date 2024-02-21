063e40f34aa53abb8c16917832fe1f4b50c15429
Akhmedovdeveloper01 <developerakhmedov@gmail.com>
2024-02-22 01:14:56 +0500

    google auth

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import { useSelector } from "react-redux";
 
 export default function App() {
+ 
   return (
     <BrowserRouter>
       <Header />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Home() {
-  return <div>Home</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  console.log(currentUser);
+  return <div>
+    <h1>Home</h1>
+    <img src={`${currentUser.avatar}`} alt="" />
+  </div>;
 }
