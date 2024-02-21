c7b7c06483395b19cb700183c55618672b91e7f0
Akhmedovdeveloper01 <developerakhmedov@gmail.com>
2024-02-21 09:30:52 +0500

    error handle

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,5 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 export default function Home() {
-  useEffect(() => {
-    axios
-      .get("http://localhost:3000/api/user/test")
-      .then((res) => console.log(res))
-      .catch((error) => console.log(error));
-  }, []);
   return <div>Home</div>;
 }
